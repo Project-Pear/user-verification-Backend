c2ce524cb897bb2b5415a8f69a9d149cf1f1e6e0
Alex Benko <benkoalex521@gmail.com>
2020-07-30 14:11:19 -0700

    Basic server set up

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 80 ;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
