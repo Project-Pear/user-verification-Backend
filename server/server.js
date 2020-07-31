3313209b543e2f6adcb3b4ee1fe5cbff584d9f49
Alex Benko <benkoalex521@gmail.com>
2020-07-31 07:05:42 -0700

    Added router to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
+const router = require('./router.js')
 const app = express();
 const port = 80 ;
 
@@ -11,5 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(morgan('dev'));
+
+app.use('/',router);
+
 
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
