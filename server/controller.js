6e8d5116f99231a79311d37373c1940b288e0bea
Alex Benko <benkoalex521@gmail.com>
2020-08-05 16:41:43 -0700

    Changes users password

--- server/controller.js
+++ server/controller.js
@@ -11,7 +11,32 @@ const controller = {
       console.log(err)
       res.status(401).send(err);
     })
+  },
+  login(req,res){
+    let {email,password} = req.body;
+
+    helpers.login(email,password)
+    .then((data)=>{
+      console.log('Successful Login!');
+      res.status(202).send(data);
+    })
+    .catch((err)=>{
+      console.log("Error Logging In:",err);
+      res.status(402).send(err)
+    })
+  },
+  changePassword(req,res){
+    helpers.changePassword(req.body.email,req.body.oldPassword,req.body.newPassword)
+    .then((data)=>{
+      console.log("Successfully Updated Users Password");
+      res.status(203).send(data);
+    })
+    .catch((err)=>{
+      console.log("Error Logging In:",err);
+      res.status(403).send(err)
+    })
   }
+
 };
 
 module.exports = controller;
