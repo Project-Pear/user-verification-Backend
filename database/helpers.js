ed396870237f6ad4fff832295b1c1107a590906d
Alex Benko <benkoalex521@gmail.com>
2020-08-05 16:39:20 -0700

    Finished function that changes a users password

--- database/helpers.js
+++ database/helpers.js
@@ -9,31 +9,53 @@ const helpers = {
     let year = date.getFullYear();
     let hashed = password.hash(req.pass);
 
-    let query = `INSERT INTO users(email,pass,firstName,lastName,joined,bDay,lives,score) VALUES('${req.email}','${hashed}','${req.firstName}','${req.lastName}',${year},'${req.bDay}','${req.lives}',${5})`
+    let query = `INSERT INTO users(email,pass,firstName,lastName,joined,bDay,lives,score) VALUES('${req.email}','${hashed}','${req.firstName}','${req.lastName}',${year},'${req.bDay}','${req.lives}',${5})`;
+
     try{
       let INSERT = await db.query(query);
-      return Promise.resolve("Added Successfully To DB..")
-
+      return Promise.resolve("Added Successfully To DB..");
     } catch(err){
       return Promise.reject(err.stack);
     }
   },
   async login(email,attemptedPassword){
-    let query = `SELECT * FROM users WHERE email = '${email}'`;
+    let query = `SELECT * FROM USERS WHERE email = '${email}';`;
 
     //login
     try{
-      let userInfo = await db.query(query);
+      let login = await db.query(query);
+      let userInfo = login.rows[0]
+      let verify = password.verify(attemptedPassword,userInfo.pass);
 
-      if(!password.verify(attemptedPassword,userInfo.pass)){
+      if(!verify){
         return Promise.reject("WRONG PASSWORD");
       } else {
         return Promise.resolve(userInfo);
       }
     }catch(err){
       //cant login, most likely not a registerd email or connection issue
-      console.log(err.stack);
-      return Promise.reject("Error With Querying:",err.stack)
+      console.log(err.stack)
+      return Promise.reject(err.stack)
+    }
+  },
+  async changePassword(email,oldPassword,newPassword){
+    let query = `SELECT * FROM USERS WHERE email = '${email}';`;
+
+    try{
+      let login = await db.query(query);
+      let userInfo = login.rows[0]
+      let verify = password.verify(attemptedPassword,userInfo.pass);
+
+      if(!verify){
+        return Promise.reject("WRONG PASSWORD");
+      } else {
+        let hashed = password.hash(newPassword)
+        let change = await db.query(`UPDATE users SET pass = '${hashed}' WHERE email = ${email};`);
+        return Promise.resolve("CHANGED PASSWORD")
+      }
+    }catch(err){
+      console.log(err.stack)
+      return Promise.reject(err.stack)
     }
   }
 
