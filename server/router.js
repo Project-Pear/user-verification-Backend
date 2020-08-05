7b9f2d9c0ecf36f4ede9686dd43e2661860fba85
Alex Benko <benkoalex521@gmail.com>
2020-08-05 00:13:27 -0700

    Added first real endpoin

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const controller = require('./controller.js');
 const router = express.Router();
 
-router.route('/test')
-.get(controller.test)
+router.route('/signup')
+.post(controller.signUp)
 
 
 
