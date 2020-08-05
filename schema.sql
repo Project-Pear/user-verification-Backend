481ace2a4e969b93197d0cf0be3132a349370a53
Alex Benko <benkoalex521@gmail.com>
2020-08-05 00:13:15 -0700

    Keeping is simple for now

--- schema.sql
+++ schema.sql
@@ -1,30 +1,13 @@
-CREATE DATABASE tinyhouse;
-
-DROP TABLE IF EXISTS users;
-
+DROP TABLE users;
 CREATE TABLE users(
   id SERIAL primary key,
   email VARCHAR(200),
   pass VARCHAR(200),
-  fullName VARCHAR(200),
+  firstName VARCHAR(200),
+  lastName VARCHAR(200),
   joined VARCHAR(200),
-  score int,
-  propId VARCHAR(200) REFERENCES stay(propId)
-)
+  bDay VARCHAR(200),
+  score int
+);
 
-CREATE TABLE stay(
-  propId VARCHAR(200) primary key,
-  title VARCHAR(200),
-  urls VARCHAR(2000),
-  descrip VARCHAR(2000),
-  rules VARCHAR(200),
-  revId int REFERENCES stayReviews(revId)
-)
-
-CREATE TABLE stayReviews(
-  revId int primary key,
-  title VARCHAR(200),
-  review VARCHAR(500),
-  recommend Boolean
-)
 
