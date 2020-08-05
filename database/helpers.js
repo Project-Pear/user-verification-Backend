5930845563aa0c3a9d7a98046abbd6e50af83036
Alex Benko <benkoalex521@gmail.com>
2020-08-05 15:32:46 -0700

    Implemented login function

--- database/helpers.js
+++ database/helpers.js
@@ -1,16 +1,39 @@
 const db = require('./index.js');
-// let query = 'SELECT * FROM TEST;';
+const password = require('./password.js');
+//.verify and .hash
+
 const helpers = {
+  //signup helpers
   async signUp(req){
     let date = new Date();
     let year = date.getFullYear();
-    let query = `INSERT INTO users(email,pass,firstName,lastName,joined,bDay,lives,score) VALUES('${req.email}','${req.pass}','${req.firstName}','${req.lastName}',${year},'${req.bDay}','${req.lives}',${5})`
+    let hashed = password.hash(req.pass);
+
+    let query = `INSERT INTO users(email,pass,firstName,lastName,joined,bDay,lives,score) VALUES('${req.email}','${hashed}','${req.firstName}','${req.lastName}',${year},'${req.bDay}','${req.lives}',${5})`
     try{
       let INSERT = await db.query(query);
       return Promise.resolve("Added Successfully To DB..")
 
     } catch(err){
       return Promise.reject(err.stack);
+    }
+  },
+  async login(email,attemptedPassword){
+    let query = `SELECT * FROM users WHERE email = '${email}'`;
+
+    //login
+    try{
+      let userInfo = await db.query(query);
+
+      if(!password.verify(attemptedPassword,userInfo.pass)){
+        return Promise.reject("WRONG PASSWORD");
+      } else {
+        return Promise.resolve(userInfo);
+      }
+    }catch(err){
+      //cant login, most likely not a registerd email or connection issue
+      console.log(err.stack);
+      return Promise.reject("Error With Querying:",err.stack)
     }
   }
 
