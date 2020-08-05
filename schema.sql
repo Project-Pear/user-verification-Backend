3f6b966211eb07221c94be2a2a083dab1213b137
Alex Benko <benkoalex521@gmail.com>
2020-08-05 11:56:02 -0700

    Added more rows and index

--- schema.sql
+++ schema.sql
@@ -1,13 +1,16 @@
 DROP TABLE users;
 CREATE TABLE users(
   id SERIAL primary key,
-  email VARCHAR(200),
+  email VARCHAR(200) UNIQUE,
   pass VARCHAR(200),
   firstName VARCHAR(200),
   lastName VARCHAR(200),
   joined VARCHAR(200),
   bDay VARCHAR(200),
+  lives VARCHAR(200),
   score int
 );
+
+CREATE INDEX ON users(id);
 
 
