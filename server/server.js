53c88ead7e864f4ffb8825d794388c1d63b8bafe
Alex Benko <benkoalex521@gmail.com>
2020-08-06 14:03:21 -0700

    Uncommenting passport.js until its actually implemented correctly

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const passport = require('passport');
 const db = require('../database/index.js');
-
 
 const router = require('./router.js')
 const app = express();
@@ -17,5 +18,8 @@ app.use(morgan('dev'));
 
 app.use('/',router);
 
+//require('./passport.js');
+//app.use(passport.initialize());
+//app.use(passport.session());
 
-app.listen(port, "0.0.0.0",() => console.log(`Listening On http://localhost:${port}`));
+app.listen(port,() => console.log(`Listening On http://localhost:${port}`));
