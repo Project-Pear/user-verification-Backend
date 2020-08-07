5a52052162703481329121e0e44297028e6e7fa3
Alex Benko <benkoalex521@gmail.com>
2020-08-07 10:25:51 -0700

    Cleaned Up

--- database/index.js
+++ database/index.js
@@ -1,7 +1,6 @@
 const secret = require('./config.js');
 const { Pool } = require('pg');
 
-
 const client = new Pool({
   user: secret.user,
   host: secret.host,
@@ -10,13 +9,12 @@ const client = new Pool({
   port:secret.port
 });
 
-
 client.connect()
-  .then((err)=>{
-    console.log('Connected To Database Successfully 👽');
-  })
-  .catch((err)=>{
-    console.log(err.stack) ;
-  });
+.then((err)=>{
+  console.log('Connected To Database Successfully 👽');
+})
+.catch((err)=>{
+  console.log(err.stack) ;
+});
 
-  module.exports = client;
+module.exports = client;
