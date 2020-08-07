d6da86848c527dbfeec607c2107110ac4bbff7cc
Alex Benko <benkoalex521@gmail.com>
2020-08-07 09:59:49 -0700

    Removed passport code

--- server/router.js
+++ server/router.js
@@ -5,8 +5,15 @@ const router = express.Router();
 router.route('/signup')
 .post(controller.signUp)
 
+
 router.route('/login')
 .get(controller.login)
+
+router.route('/profile')
+.get(require('connect-ensure-login').ensureLoggedIn(),()=>{
+  //controller.getProfileData
+})
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const passport = require('passport');
-const db = require('../database/index.js');
 
 const router = require('./router.js')
 const app = express();
@@ -17,9 +15,5 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use('/',router);
-
-//require('./passport.js');
-//app.use(passport.initialize());
-//app.use(passport.session());
 
 app.listen(port,() => console.log(`Listening On http://localhost:${port}`));
