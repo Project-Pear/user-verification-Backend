d345845e2ee25f09316208df8970b40dd12cb2d1
Alex Benko <benkoalex521@gmail.com>
2020-08-09 14:28:09 -0700

    Removed session

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
